633461b7178c0c8fae2f78615c37adc5d562445b
sasankadp <sasankadp14@gmail.com>
2024-03-08 17:51:23 -0500

    first commit

---
+++ src/main/java/org/example/cs_213_project_iii/HelloController.java
@@ -0,0 +1,14 @@
+package org.example.cs_213_project_iii;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
